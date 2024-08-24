c01a77cb78007c0a79af079431b2273b974a096f
tvizzi11k <tvizzigangbeathoe@mail.ru>
2024-08-24 15:49:19 +0300

    done reviews with button + burger menu

--- reviews.js
+++ reviews.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Логика для отправки формы
   const form = document.getElementById("contactForm");
 
   form.addEventListener("submit", (event) => {
@@ -14,32 +13,25 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(response => {
       return response.json().then(data => {
         if (response.ok && data.status === "success") {
-          console.log("Форма успешно отправлена, скрываем форму и отображаем сообщение");
           const successMessage = document.getElementById("successMessage");
           const formContainer = document.getElementById("contact-form");
 
-          // Скрываем форму сразу после успешной отправки
           formContainer.style.display = "none";
-
-          // Отображаем сообщение об успешной отправке
           successMessage.style.display = "block";
           setTimeout(() => {
-            successMessage.style.display = "none"; // Скрываем сообщение через 3 секунды
-            window.location.href = "/"; // Перенаправление на главную страницу
-          }, 3000); // Сообщение будет отображаться 3 секунды перед перенаправлением
+            successMessage.style.display = "none";
+            window.location.href = "/";
+          }, 3000);
         } else {
-          console.error("Ошибка ответа:", data);
           alert("Ошибка отправки формы: " + (data.message || "Неизвестная ошибка"));
         }
       });
     })
     .catch(error => {
-      console.error("Ошибка:", error);
       alert("Ошибка отправки формы.");
     });
   });
 
-  // Логика для открытия и закрытия формы
   const formContainer = document.getElementById("contact-form");
   const openFormButton = document.getElementById("open-form-button");
   const closeFormButton = document.getElementById("close-form");
@@ -59,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Логика для бокового меню и бургер-меню
-  const burgerMenu = document.getElementById("burger-menu");
+  const burgerMenu = document.querySelector(".frame");
   const sidebar = document.getElementById("sidebar");
   const overlay = document.getElementById("sidebar-overlay");
   const burgerMenuInside = document.querySelector('.burger-menu-inside');
@@ -81,12 +73,6 @@ document.addEventListener("DOMContentLoaded", () => {
     burgerMenu.classList.remove('active');
     sidebar.classList.remove('active');
     overlay.classList.remove('active');
-  });
-
-  // Логотип внутри боковой панели
-  const sidebarLogo = document.querySelector(".sidebar-logo");
-  sidebarLogo.addEventListener("click", () => {
-    window.location.href = "/";
   });
 
   // Логика для переходов по страницам из меню
