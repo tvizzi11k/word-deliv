f327b41941745ec3d556d918bc05e5544c4ddeb8
tvizzi11k <tvizzigangbeathoe@mail.ru>
2024-08-28 18:45:48 +0300

    done documents + done download presentation button

--- main.go
+++ main.go
@@ -6,14 +6,39 @@ import (
 	"net/http"
 	"net/smtp"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
+func downloadPresentation(w http.ResponseWriter, r *http.Request) {
+	filePath := "/Users/admin/Documents/work/word-deliv/presentation.pdf"
+	fileName := filepath.Base(filePath)
+
+	log.Printf("Попытка открыть файл: %s", filePath)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Printf("Ошибка при открытии файла: %v", err)
+		http.Error(w, "Файл не найден.", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	log.Printf("Файл найден, отправка файла: %s", fileName)
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileName))
+	w.Header().Set("Content-Type", "application/pdf")
+
+	http.ServeFile(w, r, filePath)
+}
+
 func sendEmail(to, subject, body string) error {
+	log.Println("Инициализация отправки письма.")
+
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Ошибка загрузки .env файла: %v", err)
 	}
 
 	from := os.Getenv("EMAIL_FROM")
@@ -30,6 +55,11 @@ func sendEmail(to, subject, body string) error {
 		body + "\r\n")
 
 	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, msg)
+	if err != nil {
+		log.Printf("Ошибка при отправке письма: %v", err)
+	} else {
+		log.Println("Письмо успешно отправлено.")
+	}
 	return err
 }
 
@@ -38,20 +68,21 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	// Обработка OPTIONS-запроса для CORS
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
 		return
 	}
 
-	// Обработка запроса POST
 	if r.Method == http.MethodPost {
+		log.Println("Получен POST-запрос на отправку формы.")
+
 		name := r.FormValue("name")
 		phone := r.FormValue("phone")
 		email := r.FormValue("email")
 		message := r.FormValue("message")
 
 		if name == "" || phone == "" || email == "" || message == "" {
+			log.Println("Ошибка: не все поля формы заполнены.")
 			http.Error(w, "Все поля формы обязательны к заполнению.", http.StatusBadRequest)
 			return
 		}
@@ -68,16 +99,19 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		log.Println("Форма успешно обработана.")
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, `{"status": "success", "message": "Форма успешно отправлена"}`)
 	} else {
+		log.Println("Ошибка: неправильный метод запроса.")
 		http.Error(w, "Неправильный метод запроса.", http.StatusMethodNotAllowed)
 	}
 }
 
 func main() {
 	http.HandleFunc("/submit_form", formHandler)
+	http.HandleFunc("/download", downloadPresentation)
 
 	port := os.Getenv("PORT")
 	if port == "" {
